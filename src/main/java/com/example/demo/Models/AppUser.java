0b71ede09bb9e7013c67bd133c705dbb2c87e65b
GBTC406006UR <GBTC406006UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-03-19 12:00:34 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/Models/AppUser.java
@@ -0,0 +1,132 @@
+package com.example.demo.Models;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.web.client.RestTemplate;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+
+    @NotEmpty
+    @Column(unique = true)
+    private String appUsername;
+
+    @NotEmpty
+    private String appPassword;
+
+    private String userEmail;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String personalImage;
+
+
+    @CreationTimestamp
+    Timestamp createdAt;
+
+    // Variable Getters and Setters
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAppUsername() {
+        return appUsername;
+    }
+
+    public void setAppUsername(String appUsername) {
+        this.appUsername = appUsername;
+    }
+
+    public String getAppPassword() {
+        return appPassword;
+    }
+
+    public void setAppPassword(String appPassword) {
+        this.appPassword = appPassword;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getPersonalImage() {
+        return personalImage;
+    }
+
+    public void setPersonalImage(String personalImage) {
+        this.personalImage = personalImage;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    // Connection to AppRole
+    @ManyToMany(fetch = FetchType.EAGER)
+    //This needs to be instantiated in the construtor so you can use it to add and remove individual roles
+    private Set<AppRole> roles;
+
+    public AppUser() {
+        this.roles = new HashSet<>();
+    }
+
+    public Set<AppRole> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<AppRole> roles) {
+        this.roles = roles;
+    }
+
+    public void addRole(AppRole role) {
+        this.roles.add(role);
+    }
+
+
+
+
+
+}
